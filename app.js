e3b6114e07daddfe5cece32f3d241f4d75b7bded
ebrahim <Ebrahim.Mohamed@celltek.com.eg>
2026-01-04 16:22:55 +0200

    change the port

--- app.js
+++ app.js
@@ -52,5 +52,5 @@ app.use("/payments", paymentsRoutes);
 app.use("/products", productsRoutes);
 app.use("/news", newsRoutes);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log("Server running on port", PORT));
